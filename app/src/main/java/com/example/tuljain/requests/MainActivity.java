d13f0e5efef1a6bf787ad0bbeb5fcd190b4898ac
jaingeet <geetika.jain@housing.com>
2015-12-18 00:32:18 +0530

    network call

--- app/src/main/java/com/example/tuljain/requests/MainActivity.java
+++ app/src/main/java/com/example/tuljain/requests/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tuljain.requests;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,8 +15,20 @@ import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.BufferedInputStream;
+import java.lang.Object;
+import java.net.MalformedURLException;
+import java.io.InputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-//    private DrawerLayout mdrawerlayout;
+    //    private DrawerLayout mdrawerlayout;
 //    private ListView listview;
 //    private   MyAdapter myAdapter;
 //    private android.support.v4.app.FragmentManager  fragmentManager;
@@ -27,22 +40,41 @@ public class MainActivity extends AppCompatActivity {
     private Button loginbtn;
     private Button registerbtn;
     private Button forgotpasswordbtn;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        loginbtn  = (Button) findViewById(R.id.login);
+        loginbtn = (Button) findViewById(R.id.login);
         registerbtn = (Button) findViewById(R.id.register);
         forgotpasswordbtn = (Button) findViewById(R.id.passres);
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
 
-                                         public void onClick(View v) {
-                                             Intent intentlogin = new Intent(MainActivity.this, FrontPage.class);
-                                             startActivity(intentlogin);
-                                         }
-                                     }
+                                        public void onClick(View v) {
+                                            String email = findViewById(R.id.email).toString();
+                                            String password = findViewById(R.id.pword).toString();
+                                            try {
+                                                URL url = new URL("http://localhost:3000/users/login?email=geet.com&password=geet");
+                                                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                                                System.out.println(in);
+                                            } catch (MalformedURLException e) {
+                                                System.out.println("Error: " + e.getMessage());
+                                                e.printStackTrace();
+                                            }catch(IOException e2) {
+                                                //Do something with the exception.
+                                            }
+                                            Intent intentlogin = new Intent(MainActivity.this, FrontPage.class);
+                                            startActivity(intentlogin);
+                                        }
+                                    }
 
         );
         registerbtn.setOnClickListener(new View.OnClickListener() {
@@ -90,6 +122,49 @@ public class MainActivity extends AppCompatActivity {
 //        fragmentManager.beginTransaction().replace(R.id.frameholder, myFragment0).commit();
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.tuljain.requests/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.tuljain.requests/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 
 //    private void loadSelection(int i){
