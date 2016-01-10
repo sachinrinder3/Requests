b363621f60282df6d401277b582ac09a16209036
Tulsi Jain <jaintulsi43@gmail.com>
2016-01-10 23:34:19 +0530

    one+ step ahead :)

--- app/src/main/java/com/example/android/requests/activities/Dummy.java
+++ app/src/main/java/com/example/android/requests/activities/Dummy.java
@@ -1,14 +1,19 @@
 package com.example.android.requests.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import com.example.android.requests.activities.Dummy2;
 
 import com.example.android.requests.R;
 
 public class Dummy extends AppCompatActivity {
     private Toolbar toolbar;
+    private Button dummybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,14 @@ public class Dummy extends AppCompatActivity {
         getSupportActionBar().setTitle("Dummy Activity");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        dummybutton = (Button)findViewById(R.id.dummybutton);
+        dummybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  Intent dummy2 = new Intent(Dummy.this, Dummy2.class);
+                  startActivity(dummy2);
+            }
+        });
 
     }
     @Override
