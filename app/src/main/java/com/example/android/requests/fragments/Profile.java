9b3668e3252e76dce4a3decc0bfadfa55875d9e8
Tulsi Jain <jaintulsi43@gmail.com>
2016-01-12 00:40:46 +0530

    ewchvjhvjhv

--- app/src/main/java/com/example/android/requests/fragments/Profile.java
+++ app/src/main/java/com/example/android/requests/fragments/Profile.java
@@ -71,27 +71,27 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.fragment_profile, container, false);
-        name = (AppCompatTextView)v.findViewById(R.id.user_name);
-        email = (AppCompatTextView)v.findViewById(R.id.user_email);
-        phone = (AppCompatTextView)v.findViewById(R.id.user_phone);
-        SharedPreferences sharepref = getActivity().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
-        name.setText(sharepref.getString("user_name", "nhi aaya"));
-        email.setText(sharepref.getString("user_email", "nhi aaya"));
-        phone.setText(sharepref.getString("user_phone", "nhi aaya"));
-
-
-        edit_button = (Button) v.findViewById(R.id.edit_butoon_profile);
-        edit_button.setOnClickListener(new View.OnClickListener() {
-                                           public void onClick(View v) {
-                                               EditableProfile editableProfile = new EditableProfile();
-                                               FragmentManager manager = getActivity().getSupportFragmentManager();
-                                               manager.beginTransaction().replace(R.id.frameholder, editableProfile).commit();
-
-                                           }
-                                       }
-
-        );
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+//        name = (AppCompatTextView)v.findViewById(R.id.user_name);
+//        email = (AppCompatTextView)v.findViewById(R.id.user_email);
+//        phone = (AppCompatTextView)v.findViewById(R.id.user_phone);
+//        SharedPreferences sharepref = getActivity().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+//        name.setText(sharepref.getString("user_name", "nhi aaya"));
+//        email.setText(sharepref.getString("user_email", "nhi aaya"));
+//        phone.setText(sharepref.getString("user_phone", "nhi aaya"));
+//
+//
+//        edit_button = (Button) v.findViewById(R.id.edit_butoon_profile);
+//        edit_button.setOnClickListener(new View.OnClickListener() {
+//                                           public void onClick(View v) {
+//                                               EditableProfile editableProfile = new EditableProfile();
+//                                               FragmentManager manager = getActivity().getSupportFragmentManager();
+//                                               manager.beginTransaction().replace(R.id.frameholder, editableProfile).commit();
+//
+//                                           }
+//                                       }
+//
+//        );
         return v;
 
 
