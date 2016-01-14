7171013c202360ef6970d56d987c9a16408f02cf
Tulsi Jain <jaintulsi43@gmail.com>
2016-01-14 19:27:31 +0530

    Code alignment

--- app/src/main/java/com/example/android/requests/activities/MapsActivity.java
+++ app/src/main/java/com/example/android/requests/activities/MapsActivity.java
@@ -12,7 +12,6 @@ import com.example.android.requests.utils.CustomMarker;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
--- app/src/main/java/com/example/android/requests/activities/PlaceActivity.java
+++ app/src/main/java/com/example/android/requests/activities/PlaceActivity.java
@@ -1,11 +1,7 @@
 package com.example.android.requests.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.example.android.requests.R;
 import com.google.android.gms.common.api.GoogleApiClient;
